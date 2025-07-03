d037a8dd2d861550c1b8565d5b453be99bc2b4da
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-07-03 14:06:12 +0530

    initial Structure done

---
+++ ChatFrontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-900 text-white text-2xl font-bold">
+        Real Time Chat Application
+      </div>
+    </>
+  )
+}
+
+export default App
